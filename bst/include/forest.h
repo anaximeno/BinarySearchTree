7c0a13595ac99743dbbc60362c5c7147c1196b02
anaximeno <anaximenobrito@gmail.com>
2021-06-10 20:52:40 -0100

    novos códigos adicionados

--- bst/include/forest.h
+++ bst/include/forest.h
@@ -1,5 +1,40 @@
+/**
+ * Guarda códigos de árvores binárias de pesquisa usadas no trabalho.
+*/
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <string.h>
+#include <locale.h>
+
 #ifndef FOREST_H
 #define FOREST_H
-/* Guarda códigos de árvores binárias de pesquisa usadas no trabalho. */
+
+
+
+#define R "RIGHT"
+#define L "LEFT"
+#define DEPTH_MULTIPLIER 2
+
+typedef struct _binarytree
+{
+    struct _binarytree *parent, *left, *right;
+    int valor, depth, nivel;
+    char *posicao;
+    // TODO: criar mais campos!
+} btree;
+
+/* Ponteiro para árvore binária. */
+typedef btree* broot;
+
+/* Limpa recursivamente uma árvore binária. */
+void freetree(broot *root);
+
+/**  Mostra recusivamente uma árvore binária. Tem os tipos :
+ *    -> a : em ordem,
+ *    -> b : pré ordem,
+ *    -> c : pós ordem. */
+void print_tree(btree *root, char tipo);
+
 
 #endif // FOREST_H
--- bst/src/forest.c
+++ bst/src/forest.c
@@ -1,2 +1,23 @@
-#include "forest.h"
 /* Guarda códigos de árvores binárias de pesquisa usadas no trabalho. */
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <string.h>
+#include "forest.h"
+#include "linkedlist.h"
+
+
+void freetree(broot *root)
+{
+	if (*root == NULL)
+		return ;
+
+	freetree(&(*root)->left);
+	freetree(&(*root)->right);
+
+	free(*root);
+}
+
+
+// TODO: terminar esse código!
+void print_tree(btree *root, char tipo);
