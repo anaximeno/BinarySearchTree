ea5ddfa7d4960984fde7a295df51f8c22cda0159
anaximeno <anaximenobrito@gmail.com>
2021-06-10 00:02:52 -0100

    code for linked lists

--- bst/include/linkedlist.h
+++ bst/include/linkedlist.h
@@ -2,5 +2,24 @@
 #define LINKEDLIST_H
 /* Guarda códigos de lista ligadas usadas no trabalho. */
 
+/* Estrutura de lista ligada com os campos:
+    -> next (struct _linkedlist *): próxima estrutura,
+    -> value (int): valor do nó. */
+typedef struct _linkedlist
+{
+    struct _linkedlist *next;
+    int value;
+} linked;
 
+/* cria e retorna um nó de uma lista. */
+linked *createlist(int value);
+
+/* insere um valor na lista. */
+void insert_in_list(linked **list, int value);
+
+/* retorna se um valor está ou não numa lista. */
+bool is_in_list(linked *list, int value);
+
+/* elimina (free) todos os nós de uma lista ligada. */
+void cleanlist(linked **list);
 #endif // LINKEDLIST_H
--- bst/src/linkedlist.c
+++ bst/src/linkedlist.c
@@ -1,2 +1,60 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <string.h>
 #include "linkedlist.h"
-/* Guarda códigos de lista ligadas usadas no trabalho. */
+/*  Guarda códigos de lista ligadas usadas no trabalho.
+
+    Apenas funções e/ou métodos utilizados no projeto foram
+    criados.
+*/
+
+
+linked *createlist(int value)
+{
+    linked *tmp;
+    tmp = (linked *) malloc(sizeof(linked));
+
+    if (tmp != NULL) {
+        tmp->value = value;
+        tmp->next = NULL;
+    }
+
+    return tmp;
+}
+
+
+void insert_in_list(linked **list, int value)
+{
+    if (*list == NULL)
+        *list = createlist(value);
+    else
+        insert_in_list(&(*list)->next, value);
+}
+
+
+bool is_in_list(linked *list, int value)
+{
+    linked *node = list;
+
+    while (node != NULL) {
+
+        if (node->value == value)
+            return true;
+
+        node = node->next;
+    }
+
+    return false;
+}
+
+
+void cleanlist(linked **list)
+{
+    if (*list == NULL)
+        return ;
+
+    cleanlist(&(*list)->next);
+
+    free(*list);
+}
